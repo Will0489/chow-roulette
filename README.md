3b10c18ea6fe4f0c47eb6cb0529190a020e29c6e
William <william@lunargravity.be>
2018-04-16 21:55:49 +0200

    Init

---
+++ README.md
@@ -0,0 +1,10 @@
+# Chow Roulette
+
+Sometimes you don't know what to make for dinner. Use this to inspire yourself.
+
+## Spin the wheel, choose a meal
+
+The wheel automatically divides itself into segments based on the given data.
+
+## Credits
+- [Winwheel.js by zarocknz](https://github.com/zarocknz/javascript-winwheel)
---
+++ js/main.js
@@ -0,0 +1,54 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var spinBtn = document.getElementById('spin');
+    var resetBtn = document.getElementById('reset');
+
+    console.log('Hello');
+
+    var segments = [
+        {'fillStyle': '#fff', 'text': 'Rice'},
+        {'fillStyle': '#c2d', 'text': 'Chili'},
+        {'fillStyle': '#3f3', 'text': 'Pork stew'},
+        {'fillStyle': '#9f9', 'text': 'Pizza'},
+        {'fillStyle': '#888', 'text': 'Lasagne'}
+    ];
+
+    var wheel = new Winwheel({
+        'numSegments': segments.length,
+        'outerRadius': 212,
+        'textFontSize': 28,
+        'segments': segments,
+        'animation': {
+            'type': 'spinToStop',
+            'duration' : 5,
+            'spins': 8,
+            'callbackFinished': showPrize
+        }
+    });
+
+    var wheelPower = 0;
+    var wheelSpinning = false;
+
+    function showPrize() {
+        console.log('Winner, winner chicken dinner. Or not.');
+    }
+
+    function initListeners() {
+        spinBtn.addEventListener('click', function () {
+            if (!wheelSpinning) {
+                wheel.animation.spins = 8;
+                wheel.startAnimation();
+                wheelSpinning = true;
+            }
+        });
+
+        resetBtn.addEventListener('click', function () {
+            wheel.stopAnimation(false);
+            wheel.rotationAngle = 0;
+            wheel.draw();
+
+            wheelSpinning = false;
+        })
+    }
+
+    initListeners();
+});
